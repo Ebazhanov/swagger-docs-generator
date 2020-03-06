ad525200ac43947c76091703f492a509504626fb
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-03-06 16:26:39 +0100

    - added swagger UI

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # swagger-docs-generator
+
+
+
+### Usage
+- `$ yarn start`
+- Open swagger [http://localhost:5000/api-docs/](http://localhost:5000/api-docs/)
--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const app = express();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const port = process.env.PORT || 5000;
+
+// Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Customer API',
+            description: "Customer API Information",
+            contact: {
+                name: "All you need is love"
+            },
+            servers: ["http://localhost:5000"]
+        }
+    },
+    // ['.routes/*.js']
+    apis: ["app.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
 app.get('/customers', (req, res) => {
